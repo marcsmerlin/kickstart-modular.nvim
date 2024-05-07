fd5116c586c0843d84877915af97d0c9c9114aa0
marcsmerlin <marc.s.merlin@gmail.com>
2024-05-07 19:06:33 -0400

    Disabled rustaceanvim plugin in favor of lspconig rust support.

--- lua/kickstart/plugins/rustaceanvim.lua
+++ lua/kickstart/plugins/rustaceanvim.lua
@@ -1,6 +1,7 @@
--- Recommended installation
 return {
+  --  github repository recommended installation
   'mrcjkb/rustaceanvim',
   version = '^4', -- Recommended
   lazy = false, -- This plugin is already lazy
+  enabled = false, -- Disabled to use lspconfig
 }
